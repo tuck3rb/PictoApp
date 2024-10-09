c848a889d332fbcc92df28bed3e3368fc2356cbd
tuck3rb <brocktk@hendrix.edu>
2024-10-09 16:52:16 -0500

    Major changes to the home page
    Need to split this into its own separate file next

--- pictoapp/lib/main.dart
+++ pictoapp/lib/main.dart
@@ -35,10 +35,17 @@ class MyHomePage extends StatefulWidget {
 
 class _MyHomePageState extends State<MyHomePage> {
   int _counter = 0;
+  Color _textColor = Colors.black;
 
   void _incrementCounter() {
     setState(() {
       _counter++;
+    });
+  }
+
+  void _changeTextColor(Color color) {
+    setState(() {
+      _textColor = color;
     });
   }
 
@@ -51,22 +58,97 @@ class _MyHomePageState extends State<MyHomePage> {
       ),
       body: Center(
         child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
           children: <Widget>[
+            const SizedBox(
+              height: 75,
+            ),
             const Text(
               'Select a chat room:',
+              style: TextStyle(fontSize: 20),
+            ),
+            SizedBox(
+              width: 250,
+              child: TextField(
+                decoration: const InputDecoration(
+                  border: OutlineInputBorder(),
+                  hintText: 'Display name',
+                  hintStyle: TextStyle(color: Color.fromARGB(255, 156, 156, 156)),
+                ),
+                style: TextStyle(color: _textColor, fontWeight: FontWeight.bold),
+              ),
+            ),
+            SingleChildScrollView(
+              scrollDirection: Axis.horizontal,
+              child: Row(
+                children: [
+                  _colorButton(Colors.black),
+                  _colorButton(Colors.red),
+                  _colorButton(Colors.orange),
+                  _colorButton(const Color.fromARGB(255, 255, 208, 0)),
+                  _colorButton(Colors.green),
+                  _colorButton(Colors.blue),
+                  _colorButton(Colors.purple),
+                ],
+              ),
+            ),
+            // Color option buttons here (black, red, orange, yellow, green, blue, purple)
+            // 7 boxes of different colors in this row for selecting
+            // When pressed it will change color of text in textfield for display name
+            // When enter a public or private room, this will also be the color of your display name for yourself and globally
+            // To do this it must be set up in firebase
+            SizedBox(
+              width: 250,
+              height: 100,
+              child: OutlinedButton(
+                onPressed: _incrementCounter,
+                style: OutlinedButton.styleFrom(
+                  foregroundColor: Colors.black,
+                  side: const BorderSide(color: Color.fromARGB(255, 0, 46, 83)),
+                  backgroundColor: const Color.fromARGB(255, 0, 122, 222),
+                  shape: const BeveledRectangleBorder(),
+                ),
+                child: const Text('Public Room', style: TextStyle(fontSize: 24, color: Colors.white),),
+              ),
             ),
             SizedBox(
               width: 250,
               height: 100,
-              child: ElevatedButton(
+              child: OutlinedButton(
                 onPressed: _incrementCounter,
-                child: const Text('PUBLIC ROOM'),
+                style: OutlinedButton.styleFrom(
+                  foregroundColor: Colors.black,
+                  side: const BorderSide(color: Color.fromARGB(255, 0, 46, 83)),
+                  backgroundColor: const Color.fromARGB(255, 0, 122, 222),
+                  shape: const BeveledRectangleBorder(),
+                ),
+                child: const Text('Private Room', style: TextStyle(fontSize: 24, color: Colors.white),),
               ),
+            ),
+            const SizedBox(
+              height: 100,
             ),
           ],
         ),
       ),
     );
   }
+
+  Widget _colorButton(Color color) {
+    return Padding(
+      padding: const EdgeInsets.all(4.0),
+      child: ElevatedButton(
+        onPressed: () => _changeTextColor(color),
+        style: ElevatedButton.styleFrom(
+          backgroundColor: color,
+          shape: const CircleBorder(),
+          padding: const EdgeInsets.all(12),
+        ),
+        child: Container(),
+      ),
+    );
+  }
+
 }
+
+
